d0425386b88ebbc22063db805249e49a67407951
chaharhimanshu1004 <chaharhimanshu03@gmail.com>
2025-10-03 23:57:46 +0530

    google signin implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import SignIn from './components/signin'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 function App() {
+  const GOOGLE_AUTH_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "";
   return (
     <div className="dark">
-      <SignIn />
-    </div>
+      <GoogleOAuthProvider clientId={GOOGLE_AUTH_CLIENT_ID}>
+        <SignIn />
+      </GoogleOAuthProvider>
+    </div >
   )
 }
 
